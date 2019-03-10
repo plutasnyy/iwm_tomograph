9bde62e9b981c0d3ece51c0dd724fda6aa8ed092
Kamil <Kamil.plucinski97@gmail.com>
2019-03-10 14:49:54 +0100

    fixed error during buttons render and formatted code (#6)

--- core/form2.py
+++ core/form2.py
@@ -2,65 +2,62 @@ from IPython.core.display import clear_output
 
 from core.utils.fileselector import gui_get_file_name
 import ipywidgets as widgets
-from ipywidgets import Layout, Box, Label, interactive,Text
-
-
+from ipywidgets import Layout, Box, Label, interactive, Text
 
 
 def get_form2(conf):
-
-	name = widgets.Text(
+    name = widgets.Text(
         value=conf.name,
-		placeholder='Type something',
-		description='String:',
-		disabled=False
+        placeholder='Type something',
+        description='String:',
+        disabled=False
     )
-	
-	id = widgets.Text(
+
+    id = widgets.Text(
         value=conf.id,
-		placeholder='Type something',
-		description='String:',
-		disabled=False
+        placeholder='Type something',
+        description='String:',
+        disabled=False
     )
-	
-	des = widgets.Text(
+
+    des = widgets.Text(
         value=conf.des,
-		placeholder='Type something',
-		description='String:',
-		disabled=False
+        placeholder='Type something',
+        description='String:',
+        disabled=False
     )
-	
-	filename = widgets.Text(
+
+    filename = widgets.Text(
         value=conf.filename,
-		placeholder='Type something',
-		description='String:',
-		disabled=False
+        placeholder='Type something',
+        description='String:',
+        disabled=False
     )
-	
-	process_button = widgets.Button(
+
+    process_button = widgets.Button(
         description='Save',
         disabled=False,
         tooltip='Click me',
         icon='fa-arrow-right'
     )
-	form_item_layout = Layout(
+    form_item_layout = Layout(
         display='flex',
         flex_flow='row',
         justify_content='space-between',
     )
-	
-	form_items = [
-        Box([Label(value='Name'), Text(conf.set_name, x=name)], layout=form_item_layout),
-        Box([Label(value='ID'),Text(conf.set_id, x=id)], layout=form_item_layout),
-        Box([Label(value='Description'), Text(conf.set_des, x=des)],layout=form_item_layout),
-		Box([process_button], layout=form_item_layout)
+
+    form_items = [
+        Box([Label(value='Name'), interactive(conf.set_name, x=name)], layout=form_item_layout),
+        Box([Label(value='ID'), interactive(conf.set_id, x=id)], layout=form_item_layout),
+        Box([Label(value='Description'), interactive(conf.set_des, x=des)], layout=form_item_layout),
+        Box([process_button], layout=form_item_layout)
     ]
-	
-	form = Box(form_items, layout=Layout(
+
+    form = Box(form_items, layout=Layout(
         display='flex',
         flex_flow='column',
         align_items='stretch',
         width='50%'
     ))
-	
-	return form
+
+    return form
